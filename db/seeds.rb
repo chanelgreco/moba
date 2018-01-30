c5d96c2d9d05a32c84d85e550495434b199192bf
Chanel Greco <chanel.d.greco@gmail.com>
2018-01-30 10:50:19 +0100

    Add debtors to seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+debtors = Debtor.first_or_create!([{
+    name: "Simplificator AG",
+    contact: "Sandra Blatter",
+    email: "sandra.blatter@simplificator.com",
+    expenses_limit: 1000.0
+  },
+  {
+    name: "Chanel Greco",
+    contact: "Chanel Greco",
+    email: "chanel.d.greco@gmail.com",
+    expenses_limit: 10000.0
+  }
+])
